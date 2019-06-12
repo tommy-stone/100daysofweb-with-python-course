84ff8ff0af21ff48993a4fd03f510fcae1da27ca
Tommy Stone <tommystone@gmail.com>
2019-06-12 01:31:02 -0700

    Created new script to convert file csv to json. Added app program

---
+++ days/009-012-modern-apis-starred/my_app/app.py
@@ -0,0 +1,5 @@
+import json
+from typing import List
+
+from apistar import App, Route, types, validators
+from apistar.http import JSONResponse
---
+++ days/009-012-modern-apis-starred/my_app/csv_to_json.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 11 17:44:51 2019
+
+@author: tstone
+"""
+
+#Convert csv file to json
+
+#Import data from csv file to a list file. 
+import pandas as pd
+
+#importing data from csv file 
+data = pd.read_csv('marvel-wikia-data.csv')
+
+
